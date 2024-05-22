caeb828367a57ee89688e9a6270481863c9818a8
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-05-22 19:19:19 +0300

    JavaScript giriş

---
+++ JavaScript/script.js
@@ -0,0 +1,40 @@
+alert("siteye hoşgeldiniz!");
+console.log("mrb");
+console.error("hata oluştu");
+console.warn("uyarı mesajı");
+
+
+//!  Değişkenler
+var password;
+console.log(password);
+password="fatmanur";
+console.log(password);
+
+yas=25;
+console.log(yas);
+
+//? Değişlen tanımlama kuralları
+//değişken isimleri rakam ile başlayamaz
+//komutları değişken adı olarak kullanamayız for,while
+//değişken adı oluşturulurken boşluk kullanılamaz
+//değişken isimleerinde türkçe karakter kullanılmamalı
+
+
+
+yas1=30;
+var adSoyad;
+adSoyad="Fatmanur Gebeşoğlu";
+console.log(adSoyad);
+
+var email;
+email="1234@gmail.com";
+console.log(email);
+
+email="24545@gmail.com";
+console.log(email);
+
+const sabit="3242@gm.com";
+
+//! Değişken Türleri
+
+
