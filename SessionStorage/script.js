bb5098e103a7303bc16c4977336870815cf55514
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-05-26 17:58:39 +0300

    Session storage

---
+++ SessionStorage/script.js
@@ -0,0 +1,36 @@
+
+// Tarayıcı Depolama Alanları
+
+
+// Session Storage :: oturum kapatılana kadar tutulur
+
+const add = document.querySelector("#add");
+const del = document.querySelector("#delete");
+const clear = document.querySelector("#clear");
+
+const addkey= document.querySelector("#addkey");
+const addvalue = document.querySelector("#addvalue");
+const deletekey = document.querySelector("#deletekey");
+
+add.addEventListener("click",addItem);
+del.addEventListener("click",deleteItem);
+clear.addEventListener("click",clearItem);
+
+
+function addItem(e){
+    sessionStorage.setItem(addkey.value, addvalue.value);
+}
+
+function deleteItem(e){
+    sessionStorage.removeItem(deletekey.value);
+}
+
+function clearItem(e){
+    sessionStorage.clear();
+}
+
+
+
+
+
+// Local Storage :: bilgiler javascripte silinene kadar durur kalıcıdır
