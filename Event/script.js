1b29fc358c0a7940386ab5516562ae6791aba44b
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-05-26 17:37:33 +0300

    Event bubbling ve event capturing

--- Event/script.js
+++ Event/script.js
@@ -99,5 +99,75 @@ text.addEventListener("keydown",run);
 //keyUp event
 text.addEventListener("keyup",run);
 
+//!  EVENT BUBBLING içten dışa
+
+const form = document.querySelector("form");
+const cardBody=document.querySelector(".card-body");
+const card=document.querySelector(".card");
+const container = document.querySelector(".container");
+
+form.addEventListener("click",function(e){
+    console.log("form");
+    e.stopPropagation();
+});
+
+cardBody.addEventListener("click",function(e){
+    console.log("card body");
+    e.stopPropagation();
+});
+
+card.addEventListener("click",function(e){
+    console.log("card");
+    e.stopPropagation();
+});
+
+container.addEventListener("click",function(e){
+    console.log("container");
+    e.stopPropagation();
+});
+
+
+
+//!  EVENT CAPTURING dıştan içe
+
+
+form.addEventListener("click",function(e){
+    console.log("form");
+    e.stopPropagation();
+},true);
+
+cardBody.addEventListener("click",function(e){
+    console.log("card body");
+    e.stopPropagation();
+},true);
+
+card.addEventListener("click",function(e){
+    console.log("card");
+    e.stopPropagation();
+},true);
+
+container.addEventListener("click",function(e){
+    console.log("container");
+    e.stopPropagation();
+},true);
+
+
+const deleteItems= document.querySelectorAll(".fa-times");
+
+deleteItems.forEach(function(item){
+    item.addEventListener("click",function(e){
+        console.log(e.target);
+    });
+});
+
+const listul= document.querySelector("ul");
+
+listul.addEventListener("click", function(e){
+    if(e.target.class === "fas fa-times"){
+        e.target.parentElement.parentElement.remove();
+    }
+});
+
+
 
 
