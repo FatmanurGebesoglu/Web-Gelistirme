27478eec52ea90d4a4e7a0e22b1c83196adc9431
Fatmanur Gebeşoğlu <76436684+FatmanurGebesoglu@users.noreply.github.com>
2024-05-31 15:32:44 +0300

    JS Prototype

--- NesneTabanlıProgramlama/script.js
+++ NesneTabanlıProgramlama/script.js
@@ -32,3 +32,41 @@ let Student = function (name, yearOfBirth, job) {
 
 let kaan = new Student("kaan", 2008, "student");
 let aylin = new Student("aylin", 2006, "student");
+
+
+
+//! PROTOTYPE
+
+let Person = function(name,yearOfBirth,job){
+    this.name=name;
+    this.yearOfBirth=yearOfBirth;
+    this.job=job;
+}
+
+Person.prototype.calculateAge = function(){
+     return 2024 - this.yearOfBirth;
+}
+
+Person.prototype.getName=function(){
+    return this.name;
+}
+
+let fatmanur = new Person("fatmanur",2000, "engineer")
+
+let elif = new Person("elif",2018,"student");
+
+elif.calculateAge();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
